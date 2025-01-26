203937d91b1c65e65580e902e18e61cb41a970ea
murtaza65 <102190224+murtaza65@users.noreply.github.com>
2025-01-26 15:20:41 +0000

    wo

--- src/api.jsx
+++ src/api.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL:'https://verbose-space-chainsaw-jpw4gqxv7pjhj655-5000.app.github.dev/'
 });
 
 export const fetchDonations = async () => {
   const response = await api.get('/donations');
+  console.log("Res",response)
   return response.data;
 };
 
 export const addDonation = async (donation) => {
   const response = await api.post('/donations', donation);
+  console.log("Res",response)
   return response.data;
 };
